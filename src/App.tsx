5b442a211d0ee90778bb555ca08f0c188cc058cc
echrak_ch123 <echrak234@gmail.com>
2023-06-08 11:09:53 +0100

    he

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {
 import {
   ChakraProvider,
 } from "@chakra-ui/react"
-//import "../Styles/Style.css"
+import "./Styles/Style.css"
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import KiteSurf from "./components/KiteSurf";
@@ -16,12 +16,11 @@ import Standuppaddle from "./components/Standuppaddle";
 export const App = () => (
   <ChakraProvider >
  <NavBar></NavBar> 
-
  <Routes>
-
  <Route path="/" element={<Home/>}/>
       <Route path="Home" element={<Home/>}/>
       <Route path="KiteSurf" element={<KiteSurf></KiteSurf>}/>
+      <Route path="Stand-up-paddle" element={<Standuppaddle></Standuppaddle>}/>
   </Routes>
   <Footer></Footer>
   </ChakraProvider>
